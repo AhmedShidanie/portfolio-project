5e87ff3bf63c9049393801d005bd27b4f4d7901f
Ahmed <axmaddahir25@gmail.com>
2024-12-16 22:26:03 +0300

    Fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-project/', // Set to your GitHub repository name
+  base: '/', // Set to your GitHub repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
